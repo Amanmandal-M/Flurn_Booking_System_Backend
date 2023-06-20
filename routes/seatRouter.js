9cbfa9a3bc0810742afb7c5ec62584121c44aff3
Amanmandal-M <amanmandal644@gmail.com>
2023-06-20 23:03:36 +0530

    Fixing Bugs

--- routes/seatRouter.js
+++ routes/seatRouter.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const seatRouter = express.Router();
 
-
 seatRouter.get('/seat', getSeatController);
 seatRouter.get('/seat/:id', getSeatController);
-
-
-
 
 
 module.exports = { 
